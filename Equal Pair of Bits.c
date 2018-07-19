9ce94b5291677277ec38bc047c1aa5145e565fbd
Mohamed Nabil <mohamedsadid@gmail.com>
2018-07-19 17:31:23 +0200

    Add files via upload

---
+++ Equal Pair of Bits.c
@@ -0,0 +1,30 @@
+int equalPairOfBits(int n, int m) {
+  int temp = 1;
+    int count = 0;
+
+    if (m > n) {
+        while (m > 0) {
+
+            if ((temp & n) == (temp & m)) {
+                break;
+            }
+            count++;
+
+            n >>= 1;
+            m >>= 1;
+        }
+    } else {
+        while (n > 0) {
+
+            if ((temp & n) == (temp & m)) {
+                break;
+            }
+            count++;
+
+            n >>= 1;
+            m >>= 1;
+        }
+    }
+
+    return (1 << count);
+}
